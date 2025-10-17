6b1955985dd2605d897677f4308be064be371cb3
Pavel <zacikp@seznam.cz>
2025-10-17 13:51:30 +0200

    add config

--- searchExtractor/src/app/app.config.ts
+++ searchExtractor/src/app/app.config.ts
@@ -2,18 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {providePrimeNG} from 'primeng/config';
-import {provideAnimations} from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import Aura from '@primeng/themes/aura';
-
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideAnimations(),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
       },
-    }),]
+    }),
+    provideHttpClient(),
+  ],
 };
